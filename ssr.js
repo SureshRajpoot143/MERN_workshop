1cfcb2b440cd232b0908356534ac0d0e962ee27a
Suresh Rajpoot <imssr1005@gmail.com>
2025-08-08 14:18:03 +0530

    implement anagram check function with improved logic

--- ssr.js
+++ ssr.js
@@ -463,9 +463,28 @@
 //check two word are anagrams or not
  let word1 = "listen";
  let word2 = "silent";
- if (word1.length !== word2.length) {
-        console.log("Not anagrams");
-    }
+ const h=() => {
+     if (word1.length !== word2.length) {
+         return "Not_anagram";
+     }
+    for(let i = 0; i < word1.length; i++) {
+         if (word1[i] !== word2[i]) {
+             return "Not_anagram" ;
+         }
+         for(let i of word) {
+             if (word1[i] !== word2[i]) {
+                 return "Not_anagram";
+             }
+         }
+     }
+     return "anagram";
+ };
+ console.log(h());
+                
+ 
+
+
+        
     
     
  
