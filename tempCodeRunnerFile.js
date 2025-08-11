eba8b8a22df10aad11a8084714209e6037c0d486
Suresh Rajpoot <imssr1005@gmail.com>
2025-08-11 12:19:25 +0530

    name.js

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-// let arr = [1, 2, 3, 4];
-// const rightRotateByOne = (array) => {
-//     if (array.length === 0) return array;
-//     let last = array.pop();
-//     array.unshift(last);
-//     return array;
-// };
-// console.log(rightRotateByOne(arr));
