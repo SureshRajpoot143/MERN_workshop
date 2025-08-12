16cadf2ddfda21a9a2b89cd6a17312d28cadcb6c
Suresh Rajpoot <imssr1005@gmail.com>
2025-08-12 10:27:21 +0530

    refactor: comment out destructuring examples for clarity

--- hello.js
+++ hello.js
@@ -1,8 +1,46 @@
 //destructring
 // const arr = [1, 2, 3, 4];
-const arr={"one": "hello", "two":"men", "three": 3};
-// const one = arr[0];
-const{one, two, three} = arr;
-console.log(one);
-console.log(two);
-console.log(three);
+// const arr={"one": "hello", "two":"men", "three": 3};
+// // const one = arr[0];
+// const{one, two, three} = arr;
+// console.log(one);
+// console.log(two);
+// console.log(three);
+
+// const hello=[1,2]
+// const[d,  ,f] = hello;
+// console.log(f);
+// console.log(d);
+
+// const obj={"student":"RBMI",
+//     "marks":{
+//         "math": 90,
+//         "english": 85,
+//         "science": 95   
+//     },
+//     "location":"bareilly"
+// };
+// const{student, marks:{math, english, science}, location} = obj;
+// console.log(student);
+// console.log(math);
+// console.log(english);
+// console.log(science);   
+// console.log("location =",location);     
+
+
+
+const fun=({...ages})=> {
+    console.log(ages);
+}
+fun({ages: 25,"age1": 30});
+
+
+
+
+
+
+  
+
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
-const arr = [1, 2, 3, 4];
+const obj={"student":"RBMI",
+//     "marks":{
+//         "math": 90,
+//         "english": 85,
+//         "science": 95   
+//     },
+//     "location":"bareilly"
+// };
+// const{student, marks:{math, english, science}, location} = obj;
+// console.log(student);
+// console.log(math);
+// console.log(english);
+// console.log(science);   
+// console.log("location =",location); 
